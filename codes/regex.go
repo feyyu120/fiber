e72f2a90c1ee8304bea1f4bc7c5b0e17c5cfdb68
feyyu120 <feyselyassin18@gmail.com>
2026-08-17 14:59:11 +0300

    updated

--- codes/regex.go
+++ codes/regex.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	regex := regexp.MustCompile(`^[a-zA-Z]+$`)
+	regex := regexp.MustCompile(`^[0-9]{9}$`)
 
-	fmt.Println(regex.MatchString("heLo"))
+	fmt.Println(regex.MatchString("092711861"))
 }
