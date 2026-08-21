e43fa480cf48daa5faab48a5dc945cacedcbbcca
feyyu120 <feyselfeyyu@gmail.com>
2026-08-21 20:36:17 +0300

    create user with validation

--- codes/create.go
+++ codes/create.go
@@ -1,31 +1,42 @@
 package main
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 )
 
 type Users struct {
-	Name  string `json:"name" validate:"required"`
+	Name  string `json:"name" validate:"required,min=2,max=50"`
 	Email string `json:"email" validate:"requred,email"`
+}
+
+var validate = validator.New()
+
+func Create(c fiber.Ctx) error {
+	var req Users
+	err := c.Bind().Body(&req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request",
+		})
+	}
+
+	err = validate.Struct(req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "created sucessfully",
+	})
 }
 
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
-
-	app.Post("/create", func(c fiber.Ctx) error {
-		var req Users
-		err := c.Bind().Body(&req)
-		if err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "invalid request",
-			})
-		}
-
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"message": "created sucessfully",
-		})
-	})
+	app.Post("/create", Create)
 
 }
