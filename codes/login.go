e0620bc6c1567ac5f1dda59aab1d179f579624ca
feyyu120 <feyselfeyyu@gmail.com>
2026-08-22 19:25:01 +0300

    simple login with existing credentials

--- codes/login.go
+++ codes/login.go
@@ -1,16 +1,36 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+)
 
-type User struct {
-	Email    string
-	Password string
+type Models struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Users() *Models {
+	return &Models{Email: "feyyu@gmail", Password: "feysel"}
+
 }
 
 func Login(c fiber.Ctx) error {
-
+	var req Models
+	if err := c.Bind().Body(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "ivalid request",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"msg":  "logged in succesfully",
+		"user": req.Email,
+	})
 }
 
 func main() {
-
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Post("/login", Login)
+	app.Listen(":3000", fiber.ListenConfig{DisableStartupMessage: true})
 }
